7a420f9b6ec10ef997aecea8a7c7b68f03d32442
Ronnie Guo <guorongfly@gmail.com>
2023-05-19 03:11:11 +0000

    assertEqual

---
+++ assertEqual.js
@@ -0,0 +1,16 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("\uD83D\uDE00" + " Assertion Passed: " + actual + "===" + expected);
+  } else {
+    console.log("Assertion Failed: " + actual + "!==" + expected);
+  }
+
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual(1, 2);
+assertEqual("Apple", "Apple");
+assertEqual("Apple", "Banana");
