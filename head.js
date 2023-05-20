2b6d342b38f2bbbfec8fc3d13a3b9830610d2847
Ronnie Guo <guorongfly@gmail.com>
2023-05-20 03:57:11 +0000

    head

---
+++ head.js
@@ -0,0 +1,23 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`Assertion Failed: ${actual} !== ${expected}`);
+  }
+
+};
+
+/*
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual(1, 2);
+assertEqual("Apple", "Apple");
+assertEqual("Apple", "Banana");
+*/
+//Create a function head which returns the first item in the array.
+const head = function(input) {
+  const firstItem = input[0]; 
+  return firstItem;
+}
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
