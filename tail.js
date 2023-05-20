82d4d5e5133b065297354d37069520d45f4a9f46
Ronnie Guo <guorongfly@gmail.com>
2023-05-20 17:51:13 +0000

    tail

--- tail.js
+++ tail.js
@@ -26,8 +26,7 @@ assertEqual(result[1], "Labs"); // ensure second element is "Labs"
 const words = ["Yo Yo", "Lighthouse", "Labs"];
 tail(words); // no need to capture the return value since we are not checking it
 assertEqual(words.length, 3); // original array should still have 3 elements!
-
-// Test Case: Check the original array
+// Test case: Check array with 1 item
 const yoyo = ["Yo Yo"];
-let newYoyo = tail(yoyo); // no need to capture the return value since we are not checking it
-assertEqual(newYoyo.length, 0); // original array should still have 3 elements!
+let newYoyo = tail(yoyo); 
+assertEqual(newYoyo.length, 0); 
