f551d61f610c10cd09e4944df6e4275824a88673
Ronnie Guo <guorongfly@gmail.com>
2023-05-22 21:56:43 +0000

    Only Count Specific Items

--- countOnly.js
+++ countOnly.js
@@ -14,10 +14,12 @@ const countOnly = function(allItems, itemsToCount) {
 
   for (const item of allItems) {
     console.log(item);
-    if (results[item]) {
-      results[item] += 1;
-    } else {
-      results[item] = 1;
+    if (itemsToCount[item]) { 
+      if (results[item]) {
+        results[item] += 1;
+      } else {
+        results[item] = 1;
+      }
     }
   }
 
