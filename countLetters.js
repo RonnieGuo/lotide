96d843079eec066dad60632e78d729766d7852aa
Ronnie Guo <guorongfly@gmail.com>
2023-05-23 02:47:49 +0000

    countLetters function

--- countLetters.js
+++ countLetters.js
@@ -6,3 +6,23 @@ const assertEqual = function(actual, expected) {
   }
 
 };
+
+// The function should take in a sentence (as a string) and then return a count of each of the letters in that sentence.
+const countLetters = function (input) {
+  let string = input.replace(/ /g, '');
+  //console.log(string);
+  const output = {};
+  for (const letter of string) {
+    if (output[letter]){ 
+      output[letter] += 1;
+    }
+    else {
+      output[letter] = 1;
+    }
+  }
+  return output;
+}
+
+
+console.log(countLetters("lighthouse in the house"));
+
