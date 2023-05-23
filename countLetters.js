41d22723df988c4d88fad76638acd21936f851e5
Ronnie Guo <guorongfly@gmail.com>
2023-05-23 03:00:36 +0000

    check with assertEqual

--- countLetters.js
+++ countLetters.js
@@ -20,8 +20,9 @@ const countLetters = function (input) {
       output[letter] = 1;
     }
   }
+  console.log(assertEqual(output["h"], 4));
   return output;
 }
 
 console.log(countLetters("lighthouse in the house"));
-console.log(assertEqual(countLetters("lighthouse in the house"), { l: 1, i: 2, g: 1, h: 4, t: 2, o: 2, u: 2, s: 2, e: 3, n: 1 }));
+
