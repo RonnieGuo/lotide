6fb5b401d9f251a700f668f61c60fda325e0ffeb
Ronnie Guo <guorongfly@gmail.com>
2023-05-25 22:40:39 +0000

    Use assertArraysEqual to compare

--- letterPositions.js
+++ letterPositions.js
@@ -17,5 +17,28 @@ const letterPositions = function(sentence) {
 
   return results;
 };
+
+const eqArrays = function(arr1, arr2){
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+//Implement assertArraysEqual which will take in two arrays and console.log an appropriate message to the console.
+const assertArraysEqual = function(array1, array2){
+  if (eqArrays(array1, array2)){
+   console.log("The arrays are equal.")
+  }
+  else{
+    console.log("The arrays are not equal.")
+  }
+};
 console.log(letterPositions("hel lo"));
-// assertArraysEqual(letterPositions("hello").e, [1]);
+console.log(letterPositions("lighthouse in the house"));
+
+assertArraysEqual(letterPositions("hello").e, [1]);
