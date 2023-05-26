331d6744fe6e626ac922ee1d42d6e7f325036889
Ronnie Guo <guorongfly@gmail.com>
2023-05-26 03:45:56 +0000

    check using assertEqual

--- eqObjects.js
+++ eqObjects.js
@@ -32,6 +32,9 @@ const eqObjects = function(object1, object2) {
 const shirtObject = { color: "red", size: "medium" };
 const anotherShirtObject= { size: "medium", color: "red" };
 console.log (eqObjects(shirtObject , anotherShirtObject)); // => true
+assertEqual(eqObjects(shirtObject , anotherShirtObject), false);
+
 
 const longSleeveShirtObject= { size: "medium", color: "red", sleeveLength: "long" };
 console.log(eqObjects(shirtObject , longSleeveShirtObject)); // => false
+assertEqual(eqObjects(shirtObject , longSleeveShirtObject), false);
