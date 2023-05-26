d00664fe961671fc857153bf30119371a5c5b3dc
Ronnie Guo <guorongfly@gmail.com>
2023-05-26 05:24:55 +0000

    eqObjects function

---
+++ assertObjectsEqual.js
@@ -0,0 +1,26 @@
+const eqObjects = function(object1, object2) {
+  const arr1 = Object.keys(object1);
+  const arr2 = Object.keys(object2);
+ 
+  if (arr1.length === arr2.length) {
+
+    for (const item of arr1) {
+       // check keys
+      if (!arr2.includes(item)) {
+        return false;
+      }
+      // check value
+      if (arr1[item] !== arr2[item]) {
+        return false;
+      }
+    }  
+    return true;
+  }
+  return false;
+    
+};
+
+
+const assertObjectsEqual = function(actual, expected) {
+  // Implement me!
+};
