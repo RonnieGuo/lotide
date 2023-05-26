0e9e57487e02dfd5f7278ec118a61972773bf4d3
Ronnie Guo <guorongfly@gmail.com>
2023-05-26 02:48:23 +0000

    final version

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -6,3 +6,24 @@ const assertEqual = function(actual, expected) {
   }
 
 };
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+const findKeyByValue = function (inputObject, value) {
+  const arr = Object.keys(inputObject);
+  // console.log(arr);
+  
+  for (const key of arr) {
+    // console.log(inputObject[key]);
+    if (inputObject[key] === value) {
+      return key;
+    }
+  }
+};
+
+
+console.log(findKeyByValue(bestTVShowsByGenre, "The Wire"));
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
