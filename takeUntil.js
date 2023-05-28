d35f04a9e40e18bd87ae3d3a43a013d1d47d8f62
Ronnie Guo <guorongfly@gmail.com>
2023-05-28 06:19:57 +0000

    takeUntil function

--- takeUntil.js
+++ takeUntil.js
@@ -1,6 +1,15 @@
 // Create a takeUntil function that takes in an array, and a callback function
 const takeUntil = function(array, callback) {
-  // ...
+  const results = [];
+  for (let item of array) {
+    if (!callback(item)){
+      results.push(item);
+    }
+    else{
+      break;
+    }
+  }
+  return results;
 }
 
 //Expected Input
