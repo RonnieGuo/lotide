fdd399ead71e34dd50ed29703fa36c3b490683da
Ronnie Guo <guorongfly@gmail.com>
2023-05-28 22:14:14 +0000

    Use assertEqual to write test cases for various scenarios.

--- findKey.js
+++ findKey.js
@@ -27,5 +27,16 @@ const output = findKey({
   "Ora":       { stars: 2 },
   "Akelarre":  { stars: 3 }
 }, x => x.stars === 2) // => "noma"
-
 console.log(output);
+assertEqual(output, "noma");
+
+const output3 = findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 3) // => "noma"
+console.log(output3);
+assertEqual(output3, "Akaleri");
