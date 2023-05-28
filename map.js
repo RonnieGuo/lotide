78c1e4a11f27c5b12fc575d8d9b8e553eaef367a
Ronnie Guo <guorongfly@gmail.com>
2023-05-28 03:06:27 +0000

    Implement Map

---
+++ map.js
@@ -0,0 +1,32 @@
+const words = ["ground", "control", "to", "major", "tom"];
+const map = function(array, callback) {
+  const results = [];
+  for (let item of array) {
+    results.push(callback(item));
+  }
+  return results;
+}
+const results1 = map(words, word => word[0]);
+console.log(results1);
+
+const eqArrays = function(arr1, arr2){
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+const assertArraysEqual = function(array1, array2){
+  if (eqArrays(array1, array2)){
+   console.log("The arrays are equal.")
+  }
+  else{
+    console.log("The arrays are not equal.")
+  }
+};
+
+assertArraysEqual(results1, [ 'g', 'c', 't', 'm', 't' ]); 
