1dde530bbbdd12cbe119a153dd8609800bf46706
RonnieGuo <guorongfly@gmail.com>
2023-06-09 22:24:53 -0600

    Separating assertEqual's Tests

--- assertEqual.js
+++ assertEqual.js
@@ -18,11 +18,5 @@ const assertEqual = function(actual, expected) {
 
 };
 
-// TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual(1, 2);
-assertEqual("Apple", "Apple");
-assertEqual("Apple", "Banana");
 
 module.exports = assertEqual;
