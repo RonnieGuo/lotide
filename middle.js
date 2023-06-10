e005be2afe872afa184ffd60c80a418d7de39c86
RonnieGuo <guorongfly@gmail.com>
2023-06-09 22:48:36 -0600

    Refactor middle

--- middle.js
+++ middle.js
@@ -1,23 +1,23 @@
-const eqArrays = function(arr1, arr2){
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
+// const eqArrays = function(arr1, arr2){
+//   if (arr1.length !== arr2.length) {
+//     return false;
+//   }
+//   for (let i = 0; i < arr1.length; i++) {
+//     if (arr1[i] !== arr2[i]) {
+//       return false;
+//     }
+//   }
+//   return true;
+// };
 
-const assertArraysEqual = function(array1, array2){
-  if (eqArrays(array1, array2)){
-   console.log("The arrays are equal.")
-  }
-  else{
-    console.log("The arrays are not equal.")
-  }
-};
+// const assertArraysEqual = function(array1, array2){
+//   if (eqArrays(array1, array2)){
+//    console.log("The arrays are equal.")
+//   }
+//   else{
+//     console.log("The arrays are not equal.")
+//   }
+// };
 
 const middle = function(arr){
   let arrMiddle = [];
@@ -35,12 +35,13 @@ const middle = function(arr){
   return arrMiddle;
 }
 
-console.log(middle([1])); // => []
-console.log(middle([1, 2])); // => []
-console.log(middle([1, 2, 3])); // => [2]
-console.log(middle([1, 2, 3, 4, 5])); // => [3]
-console.log(middle([1, 2, 3, 4])); // => [2, 3]
-console.log(middle([1, 2, 3, 4, 5, 6])); // => [3, 4]
+module.exports = middle;
 
-assertArraysEqual(middle([1]), []);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+
+// console.log(middle([1])); // => []
+// console.log(middle([1, 2])); // => []
+// console.log(middle([1, 2, 3])); // => [2]
+// console.log(middle([1, 2, 3, 4, 5])); // => [3]
+// console.log(middle([1, 2, 3, 4])); // => [2, 3]
+// console.log(middle([1, 2, 3, 4, 5, 6])); // => [3, 4]
+
