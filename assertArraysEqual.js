c89dcf5600e91a002277b90a5063cccb60c17145
RonnieGuo <guorongfly@gmail.com>
2023-06-09 22:44:50 -0600

    Refactor assertArraysEqual

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,14 +1,17 @@
-const eqArrays = function(arr1, arr2){
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
+// const eqArrays = function(arr1, arr2){
+//   if (arr1.length !== arr2.length) {
+//     return false;
+//   }
+//   for (let i = 0; i < arr1.length; i++) {
+//     if (arr1[i] !== arr2[i]) {
+//       return false;
+//     }
+//   }
+//   return true;
+// };
+
+const eqArrays = require('./eqArrays');
+
 //Implement assertArraysEqual which will take in two arrays and console.log an appropriate message to the console.
 const assertArraysEqual = function(array1, array2){
   if (eqArrays(array1, array2)){
@@ -19,7 +22,4 @@ const assertArraysEqual = function(array1, array2){
   }
 };
 
-assertArraysEqual([1, 2, 3], [1, 2, 3]); 
-assertArraysEqual(["1", "2", "3"], ["1", "2", 3]);
-assertArraysEqual([1, 2, 3], [3, 2, 1]);
-assertArraysEqual(["1", "2", "3"], ["1", "2", 3]);
+module.exports = assertArraysEqual;
